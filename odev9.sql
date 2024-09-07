853a0cdd98eef965faa2b8325808b200dca37349
Umut Aydın <169164758+aydinumu@users.noreply.github.com>
2024-09-07 21:43:23 +0300

    Update odev9.sql

--- odev9.sql
+++ odev9.sql
@@ -8,4 +8,4 @@
 
 SELECT city.city, country.country FROM city INNER JOIN country ON city.country_id = country.country_id;  --1
 SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id;  --2
-SELECT rental.rental_id, customer.first_name, customer.last_name FROM rentalINNER JOIN customer ON rental.customer_id = customer.customer_id; --3
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental INNER JOIN customer ON rental.customer_id = customer.customer_id; --3
